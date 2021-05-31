44ba301912f71ffecdeed7275ec0be02ad3853c4
Sergio Padrino <sergio.padrino@gmail.com>
2021-05-31 17:52:26 +0200

    Remove extra space

--- app/src/ui/drag-elements/commit-drag-element.tsx
+++ app/src/ui/drag-elements/commit-drag-element.tsx
@@ -76,7 +76,7 @@ export class CommitDragElement extends React.Component<
           <>
             {copyToPlus}
             <span>
-              <span className="copy-to">Copy to </span>
+              <span className="copy-to">Copy to</span>
               <span className="branch-name">
                 {currentDropTarget.branchName}
               </span>
